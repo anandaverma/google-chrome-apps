0d13fb79916cb780dcaaf1c61f7ea9c3f7d2a6b0
Ananda Prakash Verma <rahul.vit09@gmail.com>
2014-04-15 00:33:16 +0530

    issue: handling locations which are not found in liveweather extension #1

--- chrome-extention/liveweather/popup.js
+++ chrome-extention/liveweather/popup.js
@@ -12,9 +12,27 @@ function getWeatherByLocation() {
   var location=document.getElementById("location").value;
   if (location=="") {
     document.getElementById("data").innerHTML="please specify a valid location";
-  }
+  }  
   else {
-    var url = "http://api.openweathermap.org/data/2.5/weather?q=" + location + "&mode=html";
+    var url = "http://api.openweathermap.org/data/2.5/weather?q=" + location + "&mode=json";
+    restClient(url);
+  }
+}
+function getLocationAndWeather() {
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+      document.getElementById("result").innerHTML="Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+  var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&mode=json";
+  restClient(url);
+}
+
+function restClient(url) {
+    var xmlhttp;
     if (window.XMLHttpRequest) {
         // code for IE7+, Firefox, Chrome, Opera, Safari
         xmlhttp=new XMLHttpRequest();
@@ -27,40 +45,17 @@ function getWeatherByLocation() {
       {
       if (xmlhttp.readyState==4 && xmlhttp.status==200)
         {
-        document.getElementById("data").innerHTML=xmlhttp.responseText;
+          responseData=JSON.parse(xmlhttp.responseText);
+          if (responseData.cod==404) {
+            document.getElementById("data").innerHTML="Location Unknown";
+          } else {
+            document.getElementById("data").innerHTML="Location: " + responseData.name + "<br />" + 
+            responseData.weather[0].main + ", " + responseData.weather[0].description + "<br />Temerature: " + responseData.main.temp + "&deg;C" + 
+            "<br />Cloud: " + responseData.clouds.all + "%<br />Humidity: " + responseData.main.humidity + "%<br />Wind: " + responseData.wind.speed +
+             "m/s<br />Pressure: " + responseData.main.pressure + "hpa";
+          }
         }
       }
     xmlhttp.open("GET",url,true);
     xmlhttp.send();  
-  }
-}
-function getLocationAndWeather()
-  {
-  if (navigator.geolocation)
-    {
-    navigator.geolocation.getCurrentPosition(showPosition);
-    }
-  else{document.getElementById("result").innerHTML="Geolocation is not supported by this browser.";}
-  }
-function showPosition(position)
-  {
-var xmlhttp;
-var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&mode=html";
-if (window.XMLHttpRequest)
-  {// code for IE7+, Firefox, Chrome, Opera, Safari
-  xmlhttp=new XMLHttpRequest();
-  }
-else
-  {// code for IE6, IE5
-  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
-xmlhttp.onreadystatechange=function()
-  {
-  if (xmlhttp.readyState==4 && xmlhttp.status==200)
-    {
-    document.getElementById("data").innerHTML=xmlhttp.responseText;
-    }
-  }
-xmlhttp.open("GET",url,true);
-xmlhttp.send();  
 }
