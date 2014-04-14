f5b5bcce7e12deee4a432a66ccfdd9d27c437c05
Ananda Prakash Verma <rahul.vit09@gmail.com>
2014-04-15 02:11:42 +0530

    in-process: Add widgets for pressure and wind data #10

--- chrome-extention/liveweather/popup.js
+++ chrome-extention/liveweather/popup.js
@@ -53,9 +53,118 @@ function restClient(url) {
             responseData.weather[0].main + ", " + responseData.weather[0].description + "<br />Temerature: " + responseData.main.temp + "&deg;C" + 
             "<br />Cloud: " + responseData.clouds.all + "%<br />Humidity: " + responseData.main.humidity + "%<br />Wind: " + responseData.wind.speed +
              "m/s<br />Pressure: " + responseData.main.pressure + "hpa";
+            windGuage(responseData.main.pressure);
           }
         }
       }
     xmlhttp.open("GET",url,true);
     xmlhttp.send();  
+}
+
+function windGuage(wind) {
+  
+    $('#container').highcharts({
+  
+      chart: {
+          type: 'gauge',
+          plotBackgroundColor: null,
+          plotBackgroundImage: null,
+          plotBorderWidth: 0,
+          plotShadow: false
+      },
+      
+      title: {
+          text: 'Wind'
+      },
+      
+      pane: {
+          startAngle: -150,
+          endAngle: 150,
+          background: [{
+              backgroundColor: {
+                  linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                  stops: [
+                      [0, '#FFF'],
+                      [1, '#333']
+                  ]
+              },
+              borderWidth: 0,
+              outerRadius: '109%'
+          }, {
+              backgroundColor: {
+                  linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                  stops: [
+                      [0, '#333'],
+                      [1, '#FFF']
+                  ]
+              },
+              borderWidth: 1,
+              outerRadius: '107%'
+          }, {
+              // default background
+          }, {
+              backgroundColor: '#DDD',
+              borderWidth: 0,
+              outerRadius: '105%',
+              innerRadius: '103%'
+          }]
+      },
+         
+      // the value axis
+      yAxis: {
+          min: 0,
+          max: 2000,
+          
+          minorTickInterval: 'auto',
+          minorTickWidth: 1,
+          minorTickLength: 10,
+          minorTickPosition: 'inside',
+          minorTickColor: '#666',
+  
+          tickPixelInterval: 30,
+          tickWidth: 2,
+          tickPosition: 'inside',
+          tickLength: 10,
+          tickColor: '#666',
+          labels: {
+              step: 2,
+              rotation: 'auto'
+          },
+          title: {
+              text: 'hpa'
+          },
+          plotBands: [{
+              from: 0,
+              to: 1000,
+              color: '#55BF3B' // green
+          }, {
+              from: 1000,
+              to: 1500,
+              color: '#DDDF0D' // yellow
+          }, {
+              from: 1500,
+              to: 2000,
+              color: '#DF5353' // red
+          }]        
+      },
+  
+      series: [{
+          name: 'Wind',
+          data: [80],
+          tooltip: {
+              valueSuffix: 'hpa'
+          }
+      }]
+  
+  }, 
+  // Add some life
+  function (chart) {
+    if (!chart.renderer.forExport) {
+        setInterval(function () {
+            var point = chart.series[0].points[0],
+            newVal = wind;
+            point.update(newVal);
+        }, 2000);
+    }
+  });
 }
