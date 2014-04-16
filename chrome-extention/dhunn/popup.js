ca51115fbc38989f96ba7cdba1b5865b426ba294
Ananda Prakash Verma <rahul.vit09@gmail.com>
2014-04-17 05:19:40 +0530

    issue:Trending songs #16

--- chrome-extention/dhunn/popup.js
+++ chrome-extention/dhunn/popup.js
@@ -1,19 +1,14 @@
 // Run our script as soon as the document's DOM is ready.
 document.addEventListener('DOMContentLoaded', function () {
   getMostPopularSongs("week"); //can be - 24hrs/week/month/alltime
-  document.querySelector('button').addEventListener('click', clickHandler);
+  getHotSongs();
+  var d = new Date();
+  var n = d.getFullYear();
+  document.getElementById("credit").innerHTML="&copy<a href=\"http://hardyhawks.in\">hardyhawks.com</a> " + n + "<br />powered by <a href=\"http://gaana.com/\">Gaana.com</a>";
 });
-
-function clickHandler(e) {
-
-}
 
 function getMostPopularSongs(timeFrame) {
     var url="http://api.gaana.com?type=song&subtype=most_popular&format=json&order=" + timeFrame;
-    restClient(url);
-}
-
-function restClient(url) {
     var xmlhttp;
     if (window.XMLHttpRequest) {
         // code for IE7+, Firefox, Chrome, Opera, Safari
@@ -26,17 +21,80 @@ function restClient(url) {
     xmlhttp.onreadystatechange=function() {
       if (xmlhttp.readyState==4 && xmlhttp.status==200) {
           responseData=JSON.parse(xmlhttp.responseText);
-          reportData="<table>";
+          reportData="<h3>Popular Songs</h3><hr /><div>";
+          var rank=1;
           for (var i=0; i < responseData.tracks.length; i++) {
-              reportData=reportData+"<tr><td><img src="+responseData.tracks[i].artwork+"></td><td>"+ 
-              "<b>Title:</b> " +responseData.tracks[i].track_title+
+              reportData=reportData+"<div class=\"media\"><a class=\"pull-left\" href=\"#\"><img class=\"img-thumbnail media-object\" src="+responseData.tracks[i].artwork+"></a>"+ 
+              "<div class=\"media-body\"><span class=\"badge\">#"+ rank +"</span><br /><b>Song:</b> " +responseData.tracks[i].track_title+
               "<br /><b>Album:</b> " +responseData.tracks[i].album_title+
-              "</td></tr>";
+              "<br />";
+              if (responseData.tracks[i].video_url != "") {
+                reportData= reportData + "<b>Youtube:</b> <a href=http://"+responseData.tracks[i].video_url+" target=_blank>"+
+              responseData.tracks[i].video_url + "</a><br />";
+              }
+              reportData= reportData + "<b>Artist:</b>";
+              for (var j=0; j < responseData.tracks[i].artist.length; j++) {
+                reportData= reportData+responseData.tracks[i].artist[j].name+ " ";
+              }
+              reportData = reportData + "<br /><b>Gener:</b>";
+              for (var k=0; k < responseData.tracks[i].gener.length; k++) {
+                reportData= reportData+responseData.tracks[i].gener[k].name+ " ";
+              }
+              reportData=reportData + "</div></div><hr />";
+              rank++;
           }
-          reportData=reportData+"</table>";
-          document.getElementById("data").innerHTML=reportData;
+          reportData=reportData+"</div>";
+          document.getElementById("popular").innerHTML=reportData;
         }
       }
     xmlhttp.open("GET",url,true);
     xmlhttp.send();  
+}
+
+function getHotSongs() {
+    var url="http://api.gaana.com?type=song&subtype=hot_songs";
+    var xmlhttp;
+    if (window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    }
+    else {
+      // code for IE6, IE5
+      xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function() {
+      if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+          responseData=JSON.parse(xmlhttp.responseText);
+          reportData="<h3>Hot Songs</h3><hr /><div>";
+          var rank=1;
+          for (var i=0; i < responseData.tracks.length; i++) {
+              reportData=reportData+"<div class=\"media\"><a class=\"pull-left\" href=\"#\"><img class=\"img-thumbnail media-object\" src="+responseData.tracks[i].artwork+"></a>"+ 
+              "<div class=\"media-body\"><span class=\"badge\">#"+ rank +"</span><br /><b>Song:</b> " +responseData.tracks[i].track_title+
+              "<br /><b>Album:</b> " +responseData.tracks[i].album_title+
+              "<br />";
+              if (responseData.tracks[i].video_url != "") {
+                reportData= reportData + "<b>Youtube:</b> <a href=http://"+responseData.tracks[i].video_url+" target=_blank>"+
+              responseData.tracks[i].video_url + "</a><br />";
+              }
+              reportData= reportData + "<b>Artist:</b>";
+              for (var j=0; j < responseData.tracks[i].artist.length; j++) {
+                reportData= reportData+responseData.tracks[i].artist[j].name+ " ";
+              }
+              reportData = reportData + "<br /><b>Gener:</b>";
+              for (var k=0; k < responseData.tracks[i].gener.length; k++) {
+                reportData= reportData+responseData.tracks[i].gener[k].name+ " ";
+              }
+              reportData=reportData + "</div></div><hr />";
+              rank++;
+          }
+          reportData=reportData+"</div>";
+          document.getElementById("hot").innerHTML=reportData;
+        }
+      }
+    xmlhttp.open("GET",url,true);
+    xmlhttp.send();  
+}
+
+function restClient(url) {
+    
 }
